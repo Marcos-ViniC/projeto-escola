85b2c0cbee0c2065aa74252250156d0eaf7dbf78
Marc-ViniC <socrammarcos2003@gmail.com>
2026-01-07 03:11:46 -0200

    Primeiro Commit do projeto - projeto escola

---
+++ queries.sql
@@ -0,0 +1,21 @@
+-- Alunos filtrado por turma 
+SELECT a.nome AS Aluno, t.nome_turma AS Turma, t.turno
+FROM Alunos a
+JOIN Turmas t ON a.id_turma = t.id_turma;
+
+-- Livros disponíveis no estoque
+SELECT titulo, quantidade_disponivel
+FROM Biblioteca;
+
+-- Lista os empréstimos, mostrando quem pegou qual livro e se já devolveu
+SELECT a.nome AS Aluno, b.titulo AS Livro, e.data_emprestimo, e.data_devolucao
+FROM Emprestimos e
+JOIN Alunos a ON e.aluno_id = a.id_aluno
+JOIN Biblioteca b ON e.livro_id = b.id_livro;
+
+-- Livros mais emprestados
+SELECT b.titulo, COUNT(*) AS vezes_emprestado
+FROM Emprestimos e
+JOIN Biblioteca b ON e.livro_id = b.id_livro
+GROUP BY b.titulo
+ORDER BY vezes_emprestado DESC;
